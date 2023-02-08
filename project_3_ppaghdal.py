ff13053f3bfc429dcdd7f40ba78c81ddc12c50ec
Parth Paghdal <116045270+parth-paghdal-132@users.noreply.github.com>
2023-02-07 22:12:39 -0500

    impl project and solved bugs

--- project_3_ppaghdal.py
+++ project_3_ppaghdal.py
@@ -1,8 +1,7 @@
-import pandas
 import datetime
 from dateutil.relativedelta import relativedelta
-import prettytable as ptTable
-#17
+from prettytable import PrettyTable
+
 allowedTags = {
     'HEAD':'0', 'NOTE':'0', 'NAME':'1', 'INDI': '0', 'SEX': '1', 'BIRT':'1', 'DATE':'2', 'FAM': '0', 
     'FAMS': '1', 'FAMC': '1', 'DEAT': '1', 'HUSB':'1', 'WIFE':'1','CHIL':'1', 'MARR': '1', 'DIV':'1', 
@@ -20,23 +19,141 @@ for i in range(len(lines)):
         lines[i] = "error at line number " + i
     else:
         lines[i] = lines[i][0].strip().split(" ",2)
-        if (len(lines[i])> 2 and (lines[i][2] in ['FAM','INDI'])):
+        if(len(lines[i]) > 2 and lines[i][1] in ["INDI", "FAM"]):
+            lines[i] = "Error at line number " + i
+        elif(len(lines[i]) > 2 and lines[i][2] in ["INDI", "FAM"]):
             lines[i][1], lines[i][2] = lines[i][2], lines[i][1]
-        elif(len(lines[i])> 2 and (lines[i][1] in ['FAM','INDI'])):
-            lines[i] = "error at line number " + i
 
 infoList = []
 for i in range(len(lines)):
-    if(len(lines) == 2):
-        if((lines[i][1] in allowedTags.keys()) and allowedTags[lines[i][1]] == lines[i][0]):
-            infoList.append((lines[i][0], lines[i][1]))
-    elif(len(lines[i]) > 2):
+    if(len(lines[i]) > 2):
         if(lines[i][1] in allowedTags.keys() and allowedTags[lines[i][1]] == lines[i][0]):
             infoList.append((lines[i][0], lines[i][1], lines[i][2]))
         elif(lines[i][0:2] == "In"):
             infoList.append(lines[i])
-infoList.pop(-1)
+        else:
+            continue
+    elif(len(lines[i]) == 2):
+        if((lines[i][1] in allowedTags.keys()) and allowedTags[lines[i][1]] == lines[i][0]):
+            infoList.append((lines[i][0], lines[i][1]))
+        else:
+            continue
+
 infoList.pop(0)
+infoList.pop(-1)
 infoList = list(filter((('1', 'BIRT')).__ne__, infoList))
 
-infoList
+shouldBreak = False
+individualsData = {}
+familiesData = {}
+for i in range(len(infoList)):
+    vals = []
+    j = i + 1
+    if(infoList[i][1] == 'INDI' and infoList[i][0] == '0'):
+        while(infoList[j][1] != 'INDI'):
+            key = infoList[i][2][1:-1]
+            if(infoList[j][1] == 'FAM' and infoList[j][0] == '0'):
+                shouldBreak = True
+                break
+            elif(infoList[j][1] == 'DEAT' and infoList[j][2] == 'Y'):
+                vals.append(('DEAT', infoList[j+1][2]))
+                j += 1
+            elif(infoList[j][1] == 'FAMC' or infoList[j][1] == 'FAMS'):
+                vals.append((infoList[j][1], infoList[j][2][1:-1]))
+            else:
+                vals.append((infoList[j][1], infoList[j][2]))
+            j += 1
+        individualsData.update({key: vals})
+        if(shouldBreak):
+            break
+
+individualsTable = PrettyTable(["ID", "Name", "Gender", "Birthday", "Age", "Alive", "Death", "Child", "Spouse"])
+name = ""
+gender = ""
+birthday = ""
+death = ""
+isAlive = False
+
+for key, value in individualsData.items():
+    age = 0
+    for i in range(len(value)):
+        famc = ""
+        fams = ""
+        if(value[i][0] == "NAME"):
+            name = value[i][1]
+        if(value[i][0] == "SEX"):
+            gender = value[i][1]
+        if(value[i][0] == "DATE"):
+            birthday = datetime.datetime.strptime(value[i][1], "%d %b %Y").date()
+        if(value[i][0] == "DEAT"):
+            death = datetime.datetime.strptime(value[i][1], "%d %b %Y").date()
+        if(value[i][0] == "FAMC"):
+            famc = value[i][1]
+        if(value[i][0] == "FAMS"):
+            fams = value[i][1]
+    if(any("DEAT" in i for i in value)):
+        age = relativedelta(death, birthday).years
+        isAlive = True
+    else:
+        age = relativedelta(datetime.datetime.now(), birthday).years
+
+    tempArr = [key, name, gender, birthday, age, isAlive, death, famc, fams]
+    for i in range(len(tempArr)):
+        if(len(str(tempArr[i])) == 0):
+            tempArr[i] = "NA"
+    individualsTable.add_row(tempArr)
+
+print("Individuals")
+print(individualsTable)
+
+# shouldBreak = False
+for i in range(len(infoList)):
+    vals = []
+    j = i + 1
+    if(infoList[i][1] == 'FAM' and infoList[i][0] == '0'):
+        while(j < len(infoList)):
+            key = infoList[i][2][1:-1]
+            if(infoList[j][1] != "MARR" and infoList[j][1] != "DIV" and infoList[j][1] != "DATE" and infoList[j][1] != "FAM"):
+                vals.append((infoList[j][1], infoList[j][2][1:-1]))
+            elif(infoList[j][1] == "MARR" and len(infoList[j+1]) > 2):
+                vals.append(("MARR", infoList[j+1][2]))
+            elif(infoList[j][1] == "DIV" and len(infoList[j+1]) > 2):
+                vals.append(("DIV", infoList[j+1][2]))
+            elif(infoList[j][1] == "FAM" and infoList[j][0] == "0"):
+                # shouldBreak = True
+                break
+            j += 1
+        familiesData.update({key: vals})
+        
+husbandId = 0
+wifeId = 0
+husbandName = ""
+wifeName = ""
+famliesTable = PrettyTable(["ID", "Married", "Divorced", "Husband ID", "Husband Name", "Wife ID", "Wife Name", "Children"])
+for key, value in familiesData.items():
+    innerChild = []
+    married = ""
+    divorce = ""
+    for i in range(len(value)):
+        if(value[i][0] == "HUSB"):
+            husbandId = value[i][1]
+            husbandName = individualsData[husbandId][0][1]
+        if(value[i][0] == "WIFE"):
+            wifeId = value[i][1]
+            wifeName = individualsData[wifeId][0][1]
+        if(value[i][0] == "CHIL"):
+            child = value[i][1]
+            innerChild.append(child)
+        if(value[i][0] == "MARR"):
+            married = datetime.datetime.strptime(value[i][1],"%d %b %Y").date()
+        if(value[i][0] == "DIV"):
+            divorce = datetime.datetime.strptime(value[i][1], "%d %b %Y").date()
+    tempArr = [key, married, divorce, husbandId, husbandName, wifeId, wifeName]
+    for i in range(len(tempArr)):
+        if(len(str(tempArr[i])) == 0):
+            tempArr[i] = "NA"
+    tempArr.append(innerChild)
+    famliesTable.add_row(tempArr)
+
+print("Families")
+print(famliesTable)
