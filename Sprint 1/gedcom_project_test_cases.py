eb1677620e15c06416a71d953868146e9aacd66b
manoj-patel-d <64729767+manoj-patel-d@users.noreply.github.com>
2023-02-16 23:15:13 -0500

    Update gedcom_project_test_cases.py

--- Sprint 1/gedcom_project_test_cases.py
+++ Sprint 1/gedcom_project_test_cases.py
@@ -186,3 +186,95 @@ class TestListdeceased(unittest2.TestCase):
 
 if __name__ == '_main_':
     unittest2.main()
+# Name:- MANOJ PATEL D
+#CWID:- 20012221
+import unittest
+import project_4_sprint_1
+
+class Test11(unittest.TestCase):
+
+    def test_us_05_same_length(self):
+        usO5Data = [
+            "Error: INDIVIDUAL US05 I7: named: John /Chapmen/ Died: 1929-03-19 before married at 1959-05-10 to Maria /Chapmen/",
+            "Error: INDIVIDUAL US05 I8: named: Adrian /Chapmen/ Died: 1925-05-19 before married at 1954-06-15 to Jennifer /Chapmen/"
+        ]
+        self.assertEqual(len(us05_marriage_before_death()), len(usO5Data), "This test case will pass since assertEqual method is used and both arrays have the same length")
+
+    def test_us_05_same_content(self):
+        usO5Data = [
+            "Error: INDIVIDUAL US05 I7: named: John /Chapmen/ Died: 1929-03-19 before married at 1959-05-10 to Maria /Chapmen/",
+            "Error: INDIVIDUAL US05 I8: named: Adrian /Chapmen/ Died: 1925-05-19 before married at 1954-06-15 to Jennifer /Chapmen//"
+        ]
+        data = us05_marriage_before_death()
+        for index, value in enumerate(data):
+            self.assertEqual(value, usO5Data[index], "This test case will pass since assertEqual is used to check the content of both arrays and the content is the same")
+
+    def test_us_05_different_length(self):
+        usO5Data = [
+            "Error: INDIVIDUAL US05 I7: named: John /Chapmen/ Died: 1929-03-19 before married at 1959-05-10 to Maria /Chapmen/",
+            "Error: INDIVIDUAL US05 I8: named: Adrian /Chapmen/ Died: 1925-05-19 before married at 1954-06-15 to Jennifer /Chapmen/",
+            "Error: INDIVIDUAL US05 I8: named: Nicola /Chapmen/ Died: 1925-05-19 before married at 1930-03-16 to Peter /Chapmen/"
+        ]
+        self.assertNotEqual(len(us05_marriage_before_death()), len(usO5Data), "This test case will pass since assertNotEqual is used and both arrays have different lengths")
+
+    def test_us_05_different_content(self):
+        usO5Data = [
+            "Error: INDIVIDUAL US05 I7: named: John /Chapmen/ Died: 1929-03-19 before married at 1959-05-10 to Maria /Chapmen/",
+            "Error: INDIVIDUAL US05 I8: named: Adrian /Chapmen/ Died: 1925-05-19 before married at 1954-06-15 to Jennifer /Chapmen/"
+        ]
+        data = us05_marriage_before_death()
+        for index, value in enumerate(data):
+            self.assertNotEqual
+
+
+    # This test case will check the length of the expected and actual output
+    # This test case will pass since assertEqual is used and both arrays have the same length
+    def test_us_31_same_length(self):
+        us31Data = [
+            "Error: Individual US31 Individual id I1, name Emily /Chapman/ is living and single.",
+            "Error: Individual US31 Individual id I2, name Rachel /Chapman/ is living and single.",
+            "Error: Individual US31 Individual id I3, name Karen /Chapman/ is living and single."
+        ]
+        self.assertEqual(len(project_4_sprint_1.us31_list_living_single()), len(us31Data),
+                         "This test case will pass since assertEqual method is used and both arrays have the same length")
+
+    # This test case will check the content of the expected and actual output by order
+    # This test case will pass since assertEqual is used to check content of both arrays and that content is the same
+    def test_us_31_same_content(self):
+        us31Data = [
+            "Error: Individual US31 Individual id I1, name Emily /Chapman/ is living and single.",
+            "Error: Individual US31 Individual id I2, name Rachel /Chapman/ is living and single.",
+            "Error: Individual US31 Individual id I3, name Karen /Chapman/ is living and single."
+        ]
+        data = project_4_sprint_1.us31_list_living_single()
+        for index, value in enumerate(data):
+            self.assertEqual(value, us31Data[index],
+                             "This test case will pass since assertEqual is used to check content of both arrays and that content is the same")
+
+    # This test case will check the length of the expected and actual output
+    # This test case will pass since assertNotEqual is used and both arrays have different length
+    def test_us_31_different_length(self):
+        us31Data = [
+            "Error: Individual US31 Individual id I1, name Emily /Chapman/ is living and single.",
+            "Error: Individual US31 Individual id I2, name Rachel /Chapman/ is living and single."
+        ]
+        self.assertNotEqual(len(project_4_sprint_1.us31_list_living_single()), len(us31Data),
+                            "This test case will pass since assertNotEqual is used and both arrays have different length")
+
+    # This test case will check the content of the expected and actual output by order
+    # This test case will pass since assertNotEqual is used to check content of both arrays and that content is different
+    def test_us_31_different_content(self):
+        us31Data = [
+            "Error: Individual US31 Individual id I4, name John /Doe/ is living and single.",
+            "Error: Individual US31 Individual id I5, name Jane /Doe/ is living and single.",
+            "Error: Individual US31 Individual id I6, name Alex /Doe/ is living and single."
+        ]
+        data = project_4_sprint_1.us31_list_living_single()
+        for index, value in enumerate(data):
+            self.assertNotEqual(value, us31Data[index],
+                                "This test case will pass since assertNotEqual is used to check content of both arrays and that content is different")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
