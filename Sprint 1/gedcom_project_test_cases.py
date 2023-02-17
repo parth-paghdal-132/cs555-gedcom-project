53b082be1288059a80ffa464f190067987b608b5
Saik-21 <123621339+Saik-21@users.noreply.github.com>
2023-02-16 21:43:32 -0500

    Update gedcom_project_test_cases.py

--- Sprint 1/gedcom_project_test_cases.py
+++ Sprint 1/gedcom_project_test_cases.py
@@ -107,43 +107,43 @@ class GedComProjectTestCases(unittest.TestCase):
 #CWID:- 20010913
 #User Story-29
 import unittest2
-import user_st1 #userstory-29
+import project_4_sprint_1
 
 
-
+class TestListdeceased(unittest2.TestCase):
 
     # This test case will check the length of the expected and actual output
     # This test case will pass since assertEqual is used and both arrays have the same length
     def test_us_29_same_length(self):
         us29Data = [
-           "Error Individual List Deceased us29:name:Edward Chapmen",
-           "Error Individual List Deceased us29:name:Wanda Chapmen",
-           "Error Individual List Deceased us29:name:Sam Chapmen",
-           "Error Individual List Deceased us29:name:John Chapmen",
-           "Error Individual List Deceased us29:name:John Chapmen",
-           "Error Individual List Deceased us29:name:Adrian Chapmen",
-           "Error Individual List Deceased us29:name:Karen Chapmen",
-           "Error Individual List Deceased us29:name:Carol Chapmen",
-           "Error Individual List Deceased us29:name:Maria Chapmen"
+           "Error: Individual US29 I1 : named: Edward /Chapmen/ died on: 2010-03-15"
+            "Error: Individual US29 I2 : named: Wanda /Chapmen/ died on: 2005-06-19"
+            "Error: Individual US29 I3 : named: Sam /Chapmen/ died on: 2015-04-16"
+            "Error: Individual US29 I6 : named: John /Chapmen/ died on: 2012-02-18"
+            "Error: Individual US29 I7 : named: John /Chapmen/ died on: 1929-03-19"
+            "Error: Individual US29 I8 : named: Adrian /Chapmen/ died on: 1925-05-19"
+            "Error: Individual US29 I9 : named: Karen /Chapmen/ died on: 1990-05-18"
+            "Error: Individual US29 I11 : named: Carol /Chapmen/ died on: 2015-10-30"
+            "Error: Individual US29 I13 : named: Maria /Chapmen/ died on: 1999-05-16"
         ]
-        self.assertEqual(len(user_st1.us_29_List_deceased()), len(us29Data),
+        self.assertEqual(len(project_4_sprint_1.us_29_List_deceased()), len(us29Data),
                          "This test case will pass since assertEqual method is used and both arrays have the same length")
 
     # This test case will check the content of the expected and actual output by order
     # This test case will pass since assertEqual is used to check content of both arrays and that content is the same
     def test_us_29_same_content(self):
         us29Data = [
-            "Error Individual List Deceased us29:name:Edward Chapmen",
-           "Error Individual List Deceased us29:name:Wanda Chapmen",
-           "Error Individual List Deceased us29:name:Sam Chapmen",
-           "Error Individual List Deceased us29:name:John Chapmen",
-           "Error Individual List Deceased us29:name:John Chapmen",
-           "Error Individual List Deceased us29:name:Adrian Chapmen",
-           "Error Individual List Deceased us29:name:Karen Chapmen",
-           "Error Individual List Deceased us29:name:Carol Chapmen",
-           "Error Individual List Deceased us29:name:Maria Chapmen"
+          "Error: Individual US29 I1 : named: Edward /Chapmen/ died on: 2010-03-15"
+            "Error: Individual US29 I2 : named: Wanda /Chapmen/ died on: 2005-06-19"
+            "Error: Individual US29 I3 : named: Sam /Chapmen/ died on: 2015-04-16"
+            "Error: Individual US29 I6 : named: John /Chapmen/ died on: 2012-02-18"
+            "Error: Individual US29 I7 : named: John /Chapmen/ died on: 1929-03-19"
+            "Error: Individual US29 I8 : named: Adrian /Chapmen/ died on: 1925-05-19"
+            "Error: Individual US29 I9 : named: Karen /Chapmen/ died on: 1990-05-18"
+            "Error: Individual US29 I11 : named: Carol /Chapmen/ died on: 2015-10-30"
+            "Error: Individual US29 I13 : named: Maria /Chapmen/ died on: 1999-05-16"
         ]
-        data = user_st1.us_29_List_deceased()
+        data = project_4_sprint_1.us_29_List_deceased()
         for index, value in enumerate(data):
             self.assertEqual(value, us29Data[index],
                              "This test case will pass since assertEqual is used to check content of both arrays and that content is the same")
@@ -152,39 +152,37 @@ import user_st1 #userstory-29
     # This test case will pass since assertNotEqual is used and both arrays have different length
     def test_us_29_different_length(self):
         us29Data = [
-            "Error Individual List Deceased us29:name:Edward Chapmen",
-           "Error Individual List Deceased us29:name:Wanda Chapmen",
-           "Error Individual List Deceased us29:name:Sam Chapmen",
-           "Error Individual List Deceased us29:name:John Chapmen",
-           "Error Individual List Deceased us29:name:John Chapmen",
-           "Error Individual List Deceased us29:name:Adrian Chapmen",
-           "Error Individual List Deceased us29:name:Karen Chapmen",
-           "Error Individual List Deceased us29:name:Carol Chapmen",
-           "Error Individual List Deceased us29:name:Maria Chapmen"
+           "Error: Individual US29 I1 : named: Edward /Chapmen/ died on: 2010-03-15"
+            "Error: Individual US29 I2 : named: Wanda /Chapmen/ died on: 2005-06-19"
+            "Error: Individual US29 I3 : named: Sam /Chapmen/ died on: 2015-04-16"
+            "Error: Individual US29 I6 : named: John /Chapmen/ died on: 2012-02-18"
+            "Error: Individual US29 I7 : named: John /Chapmen/ died on: 1929-03-19"
+            "Error: Individual US29 I8 : named: Adrian /Chapmen/ died on: 1925-05-19"
+            "Error: Individual US29 I9 : named: Karen /Chapmen/ died on: 1990-05-18"
+            "Error: Individual US29 I11 : named: Carol /Chapmen/ died on: 2015-10-30"
+            "Error: Individual US29 I13 : named: Maria /Chapmen/ died on: 1999-05-16"
         ]
-        self.assertNotEqual(len(user_st1.us_29_List_deceased(),len(us29Data),"This test case will pass since assertNotEqual is used and both arrays have different length"))
+        self.assertNotEqual(len(project_4_sprint_1.us_29_List_deceased()),len(us29Data),"This test case will pass since assertNotEqual is used and both arrays have different length")
 
     # This test case will check the content of the expected and actual output by order
     # This test case will pass since assertNotEqual is used to check content of both arrays and that content is different
     def test_us_29_different_content(self):
         us29Data = [
-            "Error Individual List Deceased us29:name:Edward Chapmen",
-           "Error Individual List Deceased us29:name:Wanda Chapmen",
-           "Error Individual List Deceased us29:name:Sam Chapmen",
-           "Error Individual List Deceased us29:name:John Chapmen",
-           "Error Individual List Deceased us29:name:John Chapmen",
-           "Error Individual List Deceased us29:name:Adrian Chapmen",
-           "Error Individual List Deceased us29:name:Karen Chapmen",
-           "Error Individual List Deceased us29:name:Carol Chapmen",
-           "Error Individual List Deceased us29:name:Maria Chapmen"
+            "Error: Individual US29 I1 : named: Edward /Chapmen/ died on: 2010-03-15"
+            "Error: Individual US29 I2 : named: Wanda /Chapmen/ died on: 2005-06-19"
+            "Error: Individual US29 I3 : named: Sam /Chapmen/ died on: 2015-04-16"
+            "Error: Individual US29 I6 : named: John /Chapmen/ died on: 2012-02-18"
+            "Error: Individual US29 I7 : named: John /Chapmen/ died on: 1929-03-19"
+            "Error: Individual US29 I8 : named: Adrian /Chapmen/ died on: 1925-05-19"
+            "Error: Individual US29 I9 : named: Karen /Chapmen/ died on: 1990-05-18"
+            "Error: Individual US29 I11 : named: Carol /Chapmen/ died on: 2015-10-30"
+            "Error: Individual US29 I13 : named: Maria /Chapmen/ died on: 1999-05-16"
         ]
-        data = user_st1.us_29_List_deceased()
+        data = project_4_sprint_1.us_29_List_deceased()
         for index, value in enumerate(data):
             self.assertNotEqual(value, us29Data[index],
                                 "This test case will pass since assertNotEqual is used to check content of both arrays and that content is different")
 
 
-
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == '_main_':
+    unittest2.main()
